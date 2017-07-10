9fa8077a9be23aa90461a75955b274e366f5c43a
Ratchanon Ploynumphol <ratchanon.game@gmail.com>
2017-07-10 13:23:36 +0700

    Setup mobile navigation

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Accounts } from 'meteor/accounts-base'
 import { createContainer } from 'meteor/react-meteor-data'
+import { Session } from 'meteor/session'
 
 export const PrivateHeader = (props) => {
+    const navImageSrc = props.isNavOpen ? '/images/x.svg' : '/images/bars.svg'
     return (
         <div className='header'>
             <div className='header__content'>
+                <img className='header__nav-toggle' onClick={() => props.handleNavToggle()} src={navImageSrc} />
                 <h1 className='header__title'> {props.title} </h1>
                 <button className='button button--link-text' onClick={() => props.handleLogout()}> Logout </button>
             </div>
@@ -16,12 +19,16 @@ export const PrivateHeader = (props) => {
 
 PrivateHeader.propTypes = {
     title: PropTypes.string.isRequired,
-    handleLogout: PropTypes.func.isRequired
+    handleLogout: PropTypes.func.isRequired,
+    isNavOpen: PropTypes.bool.isRequired,
+    handleNavToggle: PropTypes.func.isRequired
 }
 
 export default createContainer(() => {
     return {
-        handleLogout: () => Accounts.logout()   // it's props of PrivateHeader
+        handleLogout: () => Accounts.logout(),   // it's props of PrivateHeader
+        handleNavToggle: () => Session.set('isNavOpen', !Session.get('isNavOpen')),
+        isNavOpen: Session.get('isNavOpen')
     }
 }, PrivateHeader)
 
